c722a76d882f79946a00489d349ba297e0868b50
Jessy SR <jessysr1991@gmail.com>
2025-01-07 23:57:10 +0900

    course_list update

--- src/app/modules/adminapp/course/course-list/course-list.module.ts
+++ src/app/modules/adminapp/course/course-list/course-list.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   ];
 
 @NgModule({
-  declarations: [CourseListComponent UserCourseComponent],
+  declarations: [CourseListComponent, UserCourseComponent],
   imports: [
     CommonModule,
     CourseRoutingModule,
