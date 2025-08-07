23bc5f90158ae8e702e3d3852a81632a8e9142ba
Jessy SR <jessysr1991@gmail.com>
2025-08-07 12:06:24 +0530

    v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'Course' }),
-    NgbModule,
-    HttpClientModule,
-    CommonModule,
-    AppRoutingModule,
-    LayoutsModule,
-    BrowserAnimationsModule,
-    CoreModule,
-    SharedModule
-  ],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule.withServerTransition({ appId: 'Course' }),
+        NgbModule,
+        CommonModule,
+        AppRoutingModule,
+        LayoutsModule,
+        BrowserAnimationsModule,
+        CoreModule,
+        SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/core/helpers/jwt.interceptor.ts
+++ src/app/core/helpers/jwt.interceptor.ts
@@ -1,12 +1,7 @@
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthenticationService } from '../../modules/auth/auth.service';
 import { Injectable } from '@angular/core';
-import { 
-    HttpRequest, 
-    HttpHandler, 
-    HttpEvent, 
-    HttpInterceptor 
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
